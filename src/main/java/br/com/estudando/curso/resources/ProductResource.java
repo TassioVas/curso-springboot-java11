05ce2cb25da60ca1a281d593959c8aef628aca26
tassiovas <t.santos.vasconcelos@gmail.com>
2022-01-19 12:52:43 -0300

    Many-to-many association with JoinTable

--- src/main/java/br/com/estudando/curso/resources/ProductResource.java
+++ src/main/java/br/com/estudando/curso/resources/ProductResource.java
@@ -21,7 +21,7 @@ public class ProductResource {
 
 	@GetMapping
 	public ResponseEntity<List<Product>> findAll(){
-		List<Product> list =  service.findAll();
+		List<Product> list =  service.findAll(); 
 		return ResponseEntity.ok().body(list);
 	}
 	
